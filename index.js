dbe5a04851026174e7868cefc6e0fef2624d8fbe
mutanim24 <mutanim24@gmail.com>
2024-06-16 12:55:42 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,62 @@
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const express = require('express')
+const cors = require("cors")
+const app = express()
+const port = 5000
+
+app.use(cors())
+app.use(express.json())
+
+
+const uri = "mongodb+srv://prime-pass:C26IPclni6F8MkXb@cluster0.z12trsh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const eventsCollection = client.db("prime-pass").collection("events");
+
+    app.get("/events", async(req, res) => {
+      const result = await eventsCollection.find().toArray();
+      res.send(result)
+    })
+
+    app.get("/events/:id", async(req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await eventsCollection.findOne(query);
+      res.send(result)
+    })
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
+// prime-pass
+// C26IPclni6F8MkXb
