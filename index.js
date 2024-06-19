4e6ce6b0ba9ad1994b43d312af43feee335a245b
mutanim24 <mutanim24@gmail.com>
2024-06-19 20:42:49 +0600

    added security

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require('express')
 const cors = require("cors")
+require('dotenv').config()
 const app = express()
 const port = 5000
 
@@ -8,7 +9,7 @@ app.use(cors())
 app.use(express.json())
 
 
-const uri = "mongodb+srv://prime-pass:C26IPclni6F8MkXb@cluster0.z12trsh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASS}@cluster0.z12trsh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
